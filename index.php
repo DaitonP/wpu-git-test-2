4f2132fd7632df3ea6302c71ce8eb1a62b625cf7
tarasiuspandito <tarasiuspandito@gmail.com>
2022-10-21 11:20:32 +0700

    menambah list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores quibusdam accusantium quisquam nesciunt commodi? Accusantium aut error quos ipsa, et fugit fugiat in reprehenderit cupiditate delectus molestiae perspiciatis totam tenetur.</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 </html>
